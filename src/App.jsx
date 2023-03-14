fb523f7513710ee68571e7768592043269f2c466
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-03-14 15:57:08 -0300

    Added task array functionality to crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { v4 as uuid } from "uuid";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+import "./App.css";
+import "./index.css";
+
+const allDetails = [
+  {
+    id: uuid().slice(4),
+    content: "Add todo",
+    checked: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useLocalStorage("allTodo", allDetails);
+  const [menu, setMenu] = useState("all");
+
+  function handleAddTodo(content) {
+    setTodos([
+      ...todos,
+      {
+        id: uuid().slice(4),
+        content: content,
+        checked: false,
+      },
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === nextTodo.id) {
+          return nextTodo;
+        } else {
+          return todo;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  }
+
+  function handleDeleteAllTodo(todoId) {
+    setTodos(todos.filter((todo) => todo.checked !== true));
+  }
+
+  function handleMenuTodo({ target }) {
+    setMenu(target.value);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header menu={menu} onHandleMenu={handleMenuTodo} />
+      <Main
+        menu={menu}
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteAllTodo={handleDeleteAllTodo}
+        onDeleteTodo={handleDeleteTodo}
+        onAddTodo={handleAddTodo}
+      />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
